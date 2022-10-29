0adc3bfdcfdda3c51ec99a422a442c0d6b18edbd
Maksym Meshkov <smarty.maks13@gmail.com>
2022-10-29 15:56:15 +0200

    HTML almost done
    First Commit

---
+++ testForContr/Controllers/AnketaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testForContr.Controllers
+{
+    public class AnketaController : Controller
+    {
+        public IActionResult Anketa()
+        {
+            return View();
+        }
+    }
+}
